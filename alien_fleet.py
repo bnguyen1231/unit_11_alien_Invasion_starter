359b24fd4ce5fa9b8500201de5efa47673201b91
BengoPop <126586966+BengoPop@users.noreply.github.com>
2025-04-13 23:50:14 -0400

    Lab 13 - Redeploy Fleet: new available configurations Checkered, grid, and cross.

--- alien_fleet.py
+++ alien_fleet.py
@@ -28,8 +28,8 @@ class AlienFleet:
         fleet_w, fleet_h = self.calculate_fleet_size(alien_w, screen_w, alien_h, screen_h)
         x_offset, y_offset = self.calculate_offsets(alien_w, alien_h, screen_w, fleet_w, fleet_h)
         
-       
-
+        #self._create_checkered_fleet(alien_w, alien_h, fleet_w, fleet_h, x_offset, y_offset)
+        #self._create_grid_shaped_fleet(alien_w, alien_h, fleet_w, fleet_h, x_offset, y_offset)
         #self._create_rectangle_fleet(alien_w, alien_h, fleet_w, fleet_h, x_offset, y_offset)
         self._create_cross_shaped_fleet(alien_w, alien_h, fleet_w, fleet_h, x_offset, y_offset)
 
@@ -66,7 +66,7 @@ class AlienFleet:
     
                 if row == fleet_h//2 or col == fleet_w//2:
                     self._create_alien(current_x,current_y)
-                self._create_alien(current_x,current_y)
+                
                 
                         
                 
